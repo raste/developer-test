230aa3093b1e021bdd6aa226a417d04dfbc34bdf
Georgi Kolev <wiraste@gmail.com>
2017-03-05 21:05:58 +0200

    Added notification message when Seller visit MyProperties page and there are no properties

--- OrangeBricks.Web/Controllers/Property/ViewModels/MyPropertiesViewModel.cs
+++ OrangeBricks.Web/Controllers/Property/ViewModels/MyPropertiesViewModel.cs
@@ -4,6 +4,8 @@ namespace OrangeBricks.Web.Controllers.Property.ViewModels
 {
     public class MyPropertiesViewModel
     {
+        public bool HasProperties { get { return Properties != null
+                                                 && Properties.Count > 0; } }
         public List<PropertyViewModel> Properties { get; set; }
     }
 }
